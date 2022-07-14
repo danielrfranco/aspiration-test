78d4dcd8804bb361a658c98b89b050a58b9f212a
Daniel Franco <danielfranco9132@gmail.com>
2022-07-13 20:17:35 -0500

    Install Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ export default function App() {
     }
   });
 
+  if (loading) return <div>'Loading...'</div>;
+  if (error) return <div>`Error! ${error.message}`</div>;
+
   return (
     <div className="App">
       {data.topic.relatedTopics.map((relatedTopic: TopicProps) => (
